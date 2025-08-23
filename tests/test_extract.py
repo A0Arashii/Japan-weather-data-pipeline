868189a480bbff3d7acf75e06b613b19db3806dc
A0Arashii <leesg040303@gmail.com>
2025-08-24 00:50:22 +0900

    test(extract): fix assertion (DataFrame.empty is a property)

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -2,7 +2,6 @@ from src.etl.extract import fetch_from_csv
 
 def test_fetch_from_csv_returns_rows():
     df = fetch_from_csv()
-    assert not df.empty()
+    assert not df.empty
     assert {"data", "city", "temp_c"}.issubset(set(map(str.lower, df.columns)))
 
-    
